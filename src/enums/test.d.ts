7166a91e7482df20be760ab89ad27c9f1c04cd0a
Hung Anh <anhlhpl52119@gmail.com>
2025-12-26 14:42:26 +0700

    chore: add migration

--- src/enums/test.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace API {
-  interface User {
-    name: string;
-    role: (typeof UserRole)[number];
-  }
-}
-const UserRole = <const>['default', 'newUser', 'blackFriday', 'promotion'];
