7c7488ff062328dc39a883d2500f50b9d6afb6c8
Hung Anh <anhlhpl52119@gmail.com>
2025-12-28 14:43:09 +0700

    chore: remove pinia

--- src/utils/common.ts
+++
@@ -1,8 +0,0 @@
-export const randomPick = (arr: any[]) => {
-  return arr[Math.floor(Math.random() * arr.length)];
-};
-export const scrollTo = (refsElement: HTMLElement, timeout?: number) => {
-  setTimeout(() => {
-    refsElement.scrollIntoView({ behavior: 'smooth' });
-  }, timeout);
-};
