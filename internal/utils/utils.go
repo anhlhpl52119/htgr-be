c924c80972147f9815481bb7d27cf61b785a4a43
Hung Anh <anhlhpl52119@gmail.com>
2026-01-02 14:48:17 +0700

    chore: response utils

---
+++ internal/utils/utils.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Envelope map[string]any
+
+func WriteJSON(w http.ResponseWriter, status int, data Envelope) error {
+	indentJson, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		return err
+	}
+
+	indentJson = append(indentJson, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(indentJson)
+	return nil
+}
+
+func GetOffset(pageNo, pageSize *int) (limit, offset int) {
+	defaultPage := 1
+	defaultSize := 10
+
+	p := defaultPage
+	if pageNo != nil && *pageNo > 0 {
+		p = *pageNo
+	}
+
+	s := defaultSize
+	if pageSize != nil && *pageSize > 0 {
+		s = *pageSize
+	}
+	limit = s
+	offset = (p - 1) * s
+
+	return limit, offset
+}
+
+func GetURLQuery(key string, r *http.Request) string {
+	return r.URL.Query().Get(key)
+}
---
+++ migrations/003_create_employees.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS employees (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID UNIQUE REFERENCES users(id) ON DELETE SET NULL, 
+    restaurant_id UUID REFERENCES restaurants(id) ON DELETE CASCADE,
+    position_id UUID REFERENCES positions(id) ON DELETE SET NULL,
+    full_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TRIGGER tr_employees_update BEFORE UPDATE ON employees FOR EACH ROW EXECUTE PROCEDURE update_updated_at_column();
+
+-- +goose Down
+DROP TABLE IF EXISTS employees CASCADE;
