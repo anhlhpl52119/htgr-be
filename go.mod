8870284e240c66661fe0c07d6038198aeb318869
Lam Hung Anh (Brim) <lamhunganh.vn@smilegate.com>
2026-04-02 14:08:13 +0700

    password

--- go.mod
+++ go.mod
@@ -18,8 +18,8 @@ require (
 	github.com/mfridman/interpolate v0.0.2 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/sethvargo/go-retry v0.3.0 // indirect
-	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/text v0.34.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
 )
 
 require (
@@ -27,6 +27,7 @@ require (
 	github.com/jackc/pgx/v5 v5.9.1
 	github.com/pressly/goose/v3 v3.27.0
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.49.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/types/user.go
+++ internal/types/user.go
@@ -1,11 +1,44 @@
 package types
 
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type UserStore interface {
 	Search() ([]User, error)
 }
+
 type User struct {
-	ID          string  `json:"id" db:"id"`
-	EmployeeID  string  `json:"employee_id" db:"employee_id"`
-	Email       float64 `json:"email" db:"email"`
-	ActualHours float64 `json:"actual_hours" db:"actual_hours"`
+	ID           string         `json:"id" db:"id"`
+	Username     string         `json:"username" db:"username"`
+	PasswordHash hashedPassword `json:"oo" db:"password_hash"`
+	IsActive     bool           `json:"is_active" db:"is_active"`
+}
+
+type hashedPassword string
+
+func (p *hashedPassword) Set(plainText string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(plainText), 12)
+	if err != nil {
+		return err
+	}
+
+	*p = hashedPassword(hashed)
+	return nil
+}
+
+func (p *hashedPassword) Matches(plainText string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(*p), []byte(plainText))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err //internal server error
+		}
+	}
+
+	return true, nil
 }
